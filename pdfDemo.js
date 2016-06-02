a72bb0993beaab8d51263a2cea1203c6bc51f626
vrushabhkaushik <vrushabhkaushik@gmail.com>
2016-06-02 15:34:26 +0530

    Update pdfDemo.js

--- pdfDemo.js
+++ pdfDemo.js
@@ -6,6 +6,13 @@ page.viewportSize = {width:800,height:600};
 page.paperSize = {format: 'Letter', orientation: 'portrait', border: '0.5in'};
 
 page.open(system.args[1], function(status){
+t = Date.now();
+z = Date.now();
+
+    while(z-t<30000)
+    {
+        z = Date.now();
+    }
     page.render(fname);
     phantom.exit();
 });
